8817d91ef9426250db955ad0059f2ebd81b0ee17
AndreiM987 <andrei_m98@yahoo.com>
2022-03-18 10:52:37 +0200

    create authentication page component alongside sign in form

--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -4,31 +4,23 @@
 ///////////
 // Comments are for the Redirect Sign In with Google
 
-import {
-  //   auth,
-  signInWithGooglePopup,
-  //   signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from "../../../utils/firebase/firebase.utils";
+import "./authentication.styles.scss";
 
 import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+import SignInForm from "../../sign-in-form/sign-in-form.component";
+
+const Authentication = () => {
   //   useEffect(async () => {
   //     const response = await getRedirectResult(auth);
   //     if (response) {
   //       const userDocRef = await createUserDocumentFromAuth(response.user);
   //     }
   //   }, []);
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
       {/* <button onClick={signInWithGoogleRedirect}>
         Sign in with Google Redirect
@@ -37,4 +29,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
           <CrownLogo className="logo"></CrownLogo>
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to="/sign-in">
+          <Link className="nav-link" to="/auth">
             Sign In
           </Link>
           <Link className="nav-link" to="/shop">
