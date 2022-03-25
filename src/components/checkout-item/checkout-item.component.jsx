8b33eb1536d82cb111ccdfca4b64f6e122e04ead
AndreiM987 <andrei_m98@yahoo.com>
2022-03-25 16:17:52 +0200

    fix increment and decrement arrow in checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -9,7 +9,7 @@ const CheckoutItem = ({ cartItem }) => {
 
   const clearItemHandler = () => clearItemFromCart(cartItem);
   const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemFromCart(cartItem);
   return (
     <div className="checkout-item-container">
       <div className="image-container">
@@ -18,11 +18,11 @@ const CheckoutItem = ({ cartItem }) => {
       <span className="name">{name}</span>
 
       <span className="quantity">
-        <div className="arrow" onClick={addItemHandler}>
+        <div className="arrow" onClick={removeItemHandler}>
           &#10094;
         </div>
         <span className="value">{quantity}</span>
-        <div className="arrow" onClick={removeItemHandler}>
+        <div className="arrow" onClick={addItemHandler}>
           &#10095;
         </div>
       </span>
